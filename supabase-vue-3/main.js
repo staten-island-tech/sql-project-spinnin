ede52d6e5d91da9dbdab9bb55cf640c4bda38320
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-05-22 13:13:17 -0400

    asdf

---
+++ supabase-vue-3/main.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+VITE_SUPABASE_URL = https://kujhpcpayjxldgwujylr.supabase.co ;
+VITE_SUPABASE_ANON_KEY =
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+    .eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1amhwY3BheWp4bGRnd3VqeWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxNjkxNTMsImV4cCI6MTk5OTc0NTE1M30
+    .RN0ZmpDP -
+  ZMSdvIvSl7CGd -
+  MvMNbI1CmeZ1Cinno4IY;
